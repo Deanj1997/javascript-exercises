664419a0da91c84590ab00c6e1c4de2c4f1e576d
Dean Johnston <25481314+Deanj1997@users.noreply.github.com>
2026-09-04 17:44:10 +0100

    Completed exercise 06
    Used for loops & string concat to solve

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,repeat) {
+    let answer = "";
 
+    if(repeat < 0){
+        answer = "ERROR";
+    }
+    else{
+        for(i = 0; i < repeat; i++){
+            answer += string;
+        }
+    }
+    return answer;
 };
 
 // Do not edit below this line
