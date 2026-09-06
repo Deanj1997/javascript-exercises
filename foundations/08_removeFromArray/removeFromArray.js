9d41fa64cc4e06d4bbb408e56b98a8b78f6fe132
Dean Johnston <25481314+Deanj1997@users.noreply.github.com>
2026-09-06 21:05:34 +0100

    Completed exercise 08
    Used the .filter method and ...args to solce the issue

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let answer = [];
+    for(const arg of args){
+        arr = arr.filter(num => num !== arg);
+    }
+    return arr;
 };
 
 // Do not edit below this line
