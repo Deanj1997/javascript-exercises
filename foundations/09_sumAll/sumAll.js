5aab7497564d9b392ff15ce83f37f0846e756efb
Dean Johnston <25481314+Deanj1997@users.noreply.github.com>
2026-09-06 21:35:21 +0100

    Completed exercise 09
    MAde sure to set the min and max and the lopp through to get the sum

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let small = 0;
+    let large = 0;
 
+    if (a < 0 || b < 0){return "ERROR"}
+    if (!Number.isInteger(a) || !Number.isInteger(b)){return "ERROR"}
+    if (a > b){
+        small = b;
+        large = a;
+    }
+    else{
+        small = a;
+        large = b;
+    }
+
+    let answer = 0;
+    for(let i = small; i <= large; i++){
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
